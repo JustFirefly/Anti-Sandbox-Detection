7f1d147ec4f573e61b29720c14106f05b610d435
JustFirefly <siddiqui.m.312@gmail.com>
2026-03-30 16:20:49 +0100

    amended vm_swap so that it is easier to change profile according to new project structure. removed RDTSC_spoof profile

--- source_code/vm_swap.sh
+++ source_code/vm_swap.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 VM_NAME="TargetVM_W11"
 
-if [ "$1" == "clean" ]; then
-    echo "[*] Swapping $VM_NAME to CLEAN profile..."
-    virsh define ./default_profile.xml
-    echo "[+] Done. Ready to launch in virt-manager."
-elif [ "$1" == "hook" ]; then
-    echo "[*] Swapping $VM_NAME to HOOKED profile..."
-    virsh define ./hooked_profile.xml
-    echo "[+] Done. Ready to launch in virt-manager."
-elif [ "$1" == "edid" ]; then
-    echo "[*] Swapping $VM_NAME to FALSE EDID profile..."
-    virsh define ./false_edid_profile.xml
-    echo "[+] Done. Ready to launch in virt-manager."
-else
-    echo "Usage: ./vm_swap.sh [clean | hook | edid]"
-fi
+# Define profile paths based on project structure
+DEFAULT_XML="./main/default_profile.xml"
+HOOK_XML="./T01/hooked_profile.xml"
+MAC_XML="./T02/mac_spoof_profile.xml"
+AUDIO_XML="./T07/audio_device_spoof.xml"
+EDID_XML="./T08/false_edid_profile.xml"
+
+case "$1" in
+    "clean")
+        echo "[*] Swapping $VM_NAME to CLEAN profile..."
+        virsh define "$DEFAULT_XML"
+        ;;
+    "hook")
+        echo "[*] Swapping $VM_NAME to HOOKED profile (KVM Hook)..."
+        virsh define "$HOOK_XML"
+        ;;
+    "mac")
+        echo "[*] Swapping $VM_NAME to MAC Spoof profile..."
+        virsh define "$MAC_XML"
+        ;;
+    "audio")
+        echo "[*] Swapping $VM_NAME to Audio Device Spoof profile..."
+        virsh define "$AUDIO_XML"
+        ;;
+    "edid")
+        echo "[*] Swapping $VM_NAME to FALSE EDID profile..."
+        virsh define "$EDID_XML"
+        ;;
+    *)
+        echo "Usage: ./vm_swap.sh [clean | hook | mac | audio | edid]"
+        exit 1
+        ;;
+esac
+
+echo "[+] Done. Ready to launch in virt-manager."
